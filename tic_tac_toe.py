e673e15b7b571dd15ee4512097c7fffff260c966
Jesung Park <jesung.park@hotmail.com>
2019-01-10 16:23:08 -0500

    Update tic_tac_toe.py

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,9 +1,48 @@
 import numpy as np
 
-def tic_tac_toe():
-    board_state = np.array([[2 2 2],[2 2 2],[2 2 2]])
-    
-    def board:
-        return board_state
+class tic_tac_toe:
+    def __init__(self):
+        self.board = np.array([[2,2,2],[2,2,2],[2,2,2]])
+        self.turn = 0
 
+    def reset(self):
+        self.board = np.array([[2,2,2],[2,2,2],[2,2,2]])
+        self.turn = 0
+
+    def move(self,x,y):
+        self.board[x,y] = self.turn
+        
+        if self.turn == 0:
+            self.turn = 1
+        else:
+            self.turn = 0
+
+    def win(self):
+        for i in range(3):
+            if self.board[0,i]==self.board[1,i]==self.board[2,i]<2:
+                return 'Won'
+            if self.board[i,0]==self.board[i,1]==self.board[i,2]<2:
+                return 'Won'
+
+        if self.board[0,0]==self.board[1,1]==self.board[2,2]<2:
+            return 'Won'
+        if self.board[0,2]==self.board[1,1]==self.board[0,2]<2:
+            return 'Won'
+
+        if not 2 in self.board:
+            return 'Drawn'
+        else:
+            return 'In-Progress'
+
+def move_gen(board_state, turn):
+    legal_moves = {}
+    
+    for i in range(3):
+        for j in range(3):
+            if board_state[i,j] == 2:
+                tmp_board = board_state.copy()
+                tmp_board[i,j] = turn
+                legal_moves[(i,j)]=tmp_board.flatten()
+
+    return legal_moves
     
